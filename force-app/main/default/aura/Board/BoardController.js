f670da1f42b2416090e0efa6f3df6f1ac70ff4c3
vinicius alves <vini90a@gmail.com>
2022-11-22 15:43:40 -0300

    board columns and rows controls added

--- force-app/main/default/aura/Board/BoardController.js
+++ force-app/main/default/aura/Board/BoardController.js
@@ -1,7 +1,25 @@
 ({
     doInit : function(component, event, helper) {
-        //contruindo uma lista de 100 palavras
-        const words = helper.getWords(6);
+        
+        //get the game mode
+        const gameMode = component.get("v.mode");
+        let column = 0;
+
+        //if gameMode != de null and hard mode
+        //get the number of columns of the game
+        if (gameMode && gameMode === 'hard') {
+            column = 6;
+        }else if (gameMode === 'medium'){
+            column = 4;
+        }else{
+            column = 3;
+        }
+
+        //get block width/size
+        let blockSize = 12/column;
+        component.set('v.blockSize', blockSize);
+        //contruindo uma lista de x palavras
+        const words = helper.getWords(column * column);
         component.set('v.words', words);
         //buscando a palavra vencedora
         const winWord = helper.getWinWord(words);
